8b38097609a52af42c4f25e0114a8d01817e6b93
falsebyte <ahadrihatim@gmail.com>
2020-06-13 16:57:30 +0200

    init spring boot project, create maven submudules

---
+++ todo-web/src/main/java/xyz/falsebit/todo/web/App.java
@@ -0,0 +1,13 @@
+package xyz.falsebit.todo.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
